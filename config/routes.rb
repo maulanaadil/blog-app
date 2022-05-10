be720b33d89f26483f880556e999480a418b041d
maulanaadil <maulanaadil.latif@gmail.com>
2022-05-10 15:06:59 +0700

    @feature/adding authentication user

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   get '/accounts', to: "accounts#index"
   devise_for :users
 
-  get '/sign_in', to: "sign_in#index"
-  post '/sign_in', to: "sign_in#create"
-  
-  get '/sign_up', to: "sign_up#index"
-  post '/sign_up', to: "sign_up#create"
-
   get "/articles", to: "articles#index"
 
   root to: "home#index"
