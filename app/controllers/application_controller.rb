4979d9b1a516c9c1b1095fddd6d51bc99f132e34
maulanaadil <maulanaadil.latif@gmail.com>
2022-05-11 13:31:18 +0700

    Set up the testing environment - Remove test directory - Add and configure rspec-rails, factory_girl_rails,   rails-controller-testing, headless, capybara, poltergeist,   database_cleaner gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+
+    def set_current_user
+        # @user = User.find(id: session[:user_id])     
+    end  
 
     def require_user_logged_in!
         redirect_to new_user_session_path, alert: "You must be signed in to do that." if current_user.nil?
