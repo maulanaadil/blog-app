4f163b9c7b5724e5f3bbf2727453d89eb9a50224
maulanaadil <maulanaadil.latif@gmail.com>
2022-05-11 16:56:01 +0700

    fix test category and user models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   context 'Associations' do
-    it 'has_many posts' do
-      association = described_class.reflect_on_association(:posts)
+    it 'has_many articles' do
+      association = described_class.reflect_on_association(:articles)
       expect(association.macro).to eq :has_many
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'Associations' do
-    it 'has_many posts' do
-      association = described_class.reflect_on_association(:posts)
+    it 'has_many articles' do
+      association = described_class.reflect_on_association(:articles)
       expect(association.macro).to eq :has_many
       expect(association.options[:dependent]).to eq :destroy
     end
