8f2c231b6dca342c499c0eff9b0bc43890503f7b
Maulana Adil <59109741+maulanaadil@users.noreply.github.com>
2022-05-11 16:39:33 +0700

    @feature/adding articles and article pages (#2)
    * @feat/adding authentication if user not login
    
    * Set up the testing environment
    - Remove test directory
    - Add and configure rspec-rails, factory_girl_rails,
      rails-controller-testing, headless, capybara, poltergeist,
      database_cleaner gems
    
    * - Generate Post and Category models.
    - Create associations between User, Post and Category models.
    - Create categories and posts database tables.
    
    * Add post and category factories
    
    * Add specs for User, Category, Post models' associations
    
    * - Add faker gem
    - Inside the seeds.rb file create methods to generate
      User, Category and Post records inside the development database
    
    * Display posts on the home page
    - Generate Posts controller and create an index action.
      Inside the index action retrieve Post records
    - Declare routes for posts
    - Create a _post.html.erb partial inside posts directory
    - Render posts inside the home page's main content
    
    * Create a show template for posts
    - Add a show action and query a post to an instance variable
    
    * adding feature etst visit single article [WIP]
    
    Co-authored-by: maulanaadil <maulanaadil.latif@gmail.com>

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,10 @@ class ArticlesController < ApplicationController
     before_action :require_user_logged_in!
 
     def index
-        
+        @articles = Article.limit(10)
     end
 
+    def show
+        @article = Article.find(params[:id])
+    end
 end    
