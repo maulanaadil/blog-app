9ee0bf4fe1a632cff6e90dc546fbd62353f65ef5
maulanaadil <maulanaadil.latif@gmail.com>
2022-05-12 14:47:59 +0700

    adding category study, hobby, team  pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,27 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def hobby
+        articles_for_branch(params[:action])
+    end
+
+    def study
+        articles_for_branch(params[:action])
+    end
+
+    def team
+        articles_for_branch(params[:action])
+    end
+
+    private
+    def articles_for_branch(branch)
+        @categories = Category.where(branch: branch)
+        @articles = get_articles.paginate(page: params[:page])
+    end
+
+    def get_articles
+        Article.limit(30)
+    end
+    
 end    
