5f1be80d2b34912c54b4d21fede2ff0c5197e151
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-05-12 12:31:38 +0700

    Add feture user management by admin

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,19 @@
 class AccountsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_user, only: [ :destroy ]
   
   def index
+    @user = User.all
+  end
+
+  def destroy 
+    @user.destroy
+    redirect_to accounts_path, notice: "User was successfully destroyed."
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
