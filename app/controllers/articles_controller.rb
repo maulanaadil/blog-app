366afcb9f2dc9271fd84adfda4c2c3e9e91c028d
maulanaadil <maulanaadil.latif@gmail.com>
2022-05-13 04:43:00 +0700

    implement feature edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,14 @@
 class ArticlesController < ApplicationController
     before_action :require_user_logged_in!
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :check_for_cancel, :only => [:create, :update]
 
     def index
         @articles = Article.limit(10).order('created_at DESC')
     end
 
     def show
-        @article = Article.find(params[:id])
+        
     end
 
     def new
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
         @category = Category.ids
     end
 
-    def create
+     def create
         @article = Article.new(article_params)
         if @article.save
             redirect_to articles_path, notice: "Tweet was scheduled successfully"
@@ -22,7 +24,23 @@ class ArticlesController < ApplicationController
             render :new 
         end  
     end
-    
+
+    def edit
+        
+    end
+
+    def update
+        if @article.update(article_params)
+            redirect_to articles_path, notice: "Article was updated successfully"
+        else
+            :edit
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path, alert: "Article was deleted"
+    end    
 
     def hobby
         articles_for_branch(params[:action])
@@ -48,6 +66,18 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:content, :title, :category_id).merge(user_id: current_user.id)
-    end   
+    end
+    
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+
+    def check_for_cancel
+        if params[:commit] == "Cancel"
+            redirect_to my_page_path
+        end
+    end
+    
     
 end    
