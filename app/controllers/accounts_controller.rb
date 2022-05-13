17ac246777921500de7c5151bf48b2c265c1e9fb
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-05-13 09:21:40 +0700

    Fix function of CRUD user by admin

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,15 @@
 class AccountsController < ApplicationController
   before_action :require_user_logged_in!
-  before_action :set_user, only: [ :destroy ]
+  before_action :set_user, only: [ :show, :edit, :destroy ]
   
   def index
     @user = User.all
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def destroy 
