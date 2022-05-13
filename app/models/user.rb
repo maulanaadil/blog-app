6a041a439ba936ce573ad50ad4887484fc7a8875
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-05-13 12:40:48 +0700

    Add user model moderator

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: [:user, :admin]
+  enum role: [:user, :moderator, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
