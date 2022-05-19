8c53f2185500df3778b8fbfd7a42abeda515c45e
maulanaadil <maulanaadil.latif@gmail.com>
2022-05-19 15:03:52 +0700

    @test/implement feature test user role admin can delete article

--- spec/features/articles/delete_article_spec.rb
+++ spec/features/articles/delete_article_spec.rb
@@ -3,9 +3,10 @@ require 'pry'
 
 RSpec.feature "Visit single article and delete it", :type => :feature do
   let(:user) { create(:user) }
+  let(:admin) { create(:user, role: 'admin') }
   let(:article) { create(:article) }
 
-  scenario "User goes to a single article from the home page then go delete for the article", js: true do
+  scenario "User as User role goes to a single article from the home page then can't go delete for the article", js: true do
     article
     user
     visit root_path
@@ -19,7 +20,24 @@ RSpec.feature "Visit single article and delete it", :type => :feature do
     
     find('a[href="/articles/1"]').click
 
+    expect(page).not_to have_selector('button.button-delete')
+  end
+
+  scenario "User as Admin role goes to a single article from the home page then can go delete for the article", js: true do
+    article
+    admin
+    visit root_path
+    
+    find('nav a', text: 'Sign In').click
+    fill_in 'user[email]', with: admin.email
+    fill_in 'user[password]', with: admin.password
+    find('.login-button').click
+    
+    find('nav a', text: 'Articles').click
+    
+    find('a[href="/articles/1"]').click
     find('button.button-delete').click
     expect(page).not_to have_selector('#single-article-content p', text: article.content)
   end
+
 end
