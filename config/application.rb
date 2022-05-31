ee34baf0696d0c4ee64d1334a1ee2de5ef0c0c97
maulanaadil <maulanaadil.latif@gmail.com>
2022-05-31 13:42:26 +0700

    change static text into locales .yml

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module BlogApp
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.i18n.available_locales = [:en, :id]
+
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
