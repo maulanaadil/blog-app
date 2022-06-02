757041d7be783ac8888d0015b24fee0990eae2f1
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-06-02 10:45:50 +0700

    Add un happy test for sign in and sign up page

--- spec/features/user/sign_in_spec.rb
+++ spec/features/user/sign_in_spec.rb
@@ -5,12 +5,53 @@ RSpec.feature "Login", :type => :feature do
 
   scenario 'user navigates to the login page and succesfully logs in', js: true do
     user
-    visit root_path
-    find('nav a', text: 'Sign In').click
+    visit new_user_session_path
+
     fill_in 'user[email]', with: user.email
     fill_in 'user[password]', with: user.password
     find('.login-button').click
-    expect(page).to have_selector('#user-settings')
+
+    expect(page).to have_text('Homepage')
+    expect(page).to have_text('Signed in successfully.')
   end
 
+  scenario 'user navigates to the login page and try input a wrong password', js: true do
+    user
+    visit new_user_session_path
+
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: "mock password"
+    find('.login-button').click
+
+    expect(page).to have_text('Invalid Email or password.')
+    expect(page).to have_text('Sign In')
+    expect(page).to have_text('Email')
+    expect(page).to have_text('Password')
+  end
+
+  scenario 'user navigates to the login page and try input a wrong email', js: true do
+    user
+    visit new_user_session_path
+
+    fill_in 'user[email]', with: "mock email"
+    fill_in 'user[password]', with: user.password
+    find('.login-button').click
+
+    expect(page).to have_text('Invalid Email or password.')
+    expect(page).to have_text('Sign In')
+    expect(page).to have_text('Email')
+    expect(page).to have_text('Password')
+  end
+
+  scenario 'user navigates to the login page and try to click sign up button', js: true do
+    user
+    visit new_user_session_path
+
+    find('a[href="/users/sign_up"]').click
+    
+    expect(page).to have_text('Sign up')
+    expect(page).to have_text('Email')
+    expect(page).to have_text('Password')
+    expect(page).to have_text('Password confirmation')
+  end
 end
--- spec/features/user/sign_up_spec.rb
+++ spec/features/user/sign_up_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.feature "Sign up", :type => :feature do
   let(:user) { build(:user) }
+  let(:another_user) { create(:user, email: "mock@email.com") }
 
   scenario 'user navigates to sign up page and successfully signs up', js: true do
     visit root_path
@@ -9,8 +10,46 @@ RSpec.feature "Sign up", :type => :feature do
     fill_in 'user[email]', with: user.email
     fill_in 'user[password]', with: user.password
     fill_in 'user[password_confirmation]', with: user.password_confirmation
+
     find('.sign-up-button').click
+
     expect(page).to have_selector('#user-settings')
+    expect(page).to have_text('Welcome! You have signed up successfully.')
   end
 
+  scenario 'user navigates to sign up page and try to input password that doesn\'t match password', js: true do
+    visit root_path
+    find('nav a', text: 'Sign Up').click
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: "mock confirm password"
+
+    find('.sign-up-button').click
+
+    expect(page).to have_text('Password confirmation doesn\'t match Password')
+  end
+
+  scenario 'user navigates to sign up page and try to input email that already been taken', js: true do
+    visit root_path
+    find('nav a', text: 'Sign Up').click
+    fill_in 'user[email]', with: "mock@email.com"
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password_confirmation
+
+    find('.sign-up-button').click
+
+    expect(page).to have_text('Email has already been taken')
+  end
+
+  scenario 'user navigates to sign up page and try to input password and confirm password that contains less than 6 characters', js: true do
+    visit root_path
+    find('nav a', text: 'Sign Up').click
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: "12345"
+    fill_in 'user[password_confirmation]', with: "12345"
+
+    find('.sign-up-button').click
+
+    expect(page).to have_text('Password is too short (minimum is 6 characters)')
+  end
 end
